9ea203ad22bc9d9f37d1aa64ce77c29285f61b3c
Nik <mrpepsi98@gmail.com>
2025-02-18 21:24:53 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+<h2>Aviasales App</h2>
+[Deploy link]()
---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import cl from './App.module.scss'
+
+function App() {
+  return (
+    <>
+        <header>
+            Aviasales Logo
+        </header>
+        <main className={cl.main}>
+            Main Container
+        </main>
+        <footer>
+            Footer
+        </footer>
+    </>
+  )
+}
+
+export default App
